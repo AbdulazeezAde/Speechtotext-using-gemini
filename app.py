947d250573e264da5107ac02ea208cf7d9de5f4c
Abdulazeez <41235090+AbdulazeezAde@users.noreply.github.com>
2025-07-20 15:03:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ st.markdown("Record or upload audio to transcribe using **Gemini API**.")
 
 gemini_key = st.text_input("🔑 Enter your Gemini API Key:", type="password")
 
-audio_data = st.audio(label="🎤 Record or Upload Your Audio", format="audio/wav")
+#audio_data = st.audio(label="🎤 Record or Upload Your Audio", format="audio/wav")
+audio_data = st.audio_input("🎙️ Record your answer")
+audio_data = st.audio(audio_data, format='audio/wav')
 
 if audio_data and gemini_key:
     with st.spinner("Transcribing..."):
